667b99ce2672f8aa8593d58bc208752e9f3e2859
jackmatson19 <38259378+jackmatson19@users.noreply.github.com>
2018-05-13 22:24:15 -0700

    completed the themes but images need to be found, rolling background was attempted but is not compatable with game, blocks off obstacles

--- main.py
+++ main.py
@@ -1,60 +1,94 @@
-#week 15: worked on finding game menu and altered settings
+##created by JACK MATSON '19 and TRISTAN SHEETZ '19
 
 # code taken from F-Prime  at https://github.com/f-prime/FlappyBird
-
+# rolling background from here https://youtu.be/US3HSusUBeI
 #importing libraries
 import pygame 
 from pygame.locals import *  
 import sys
 import random
-import time
+import os
 
-#code typed from youtube video 
-#currently not wroking 
-#working with another set of code for flappy bird from Github that came with a starting menu 
-#goal is to be able to have a main game menu next week 
-def game_intro():
-    intro = True
-    while intro:
-        for event in pygame.event.get():
-            print(event)
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()    
-        gameDisplay.fill(white)
-        largeText = pygame.font.Font('freesansbold.ttf',115)
-        TextSurf, TextRect = text_objects("A bit Racey", largeText)
-        TextRect.center = ((display_width/2),(display_height/2))
-        gameDisplay.blit(TextSurf, TextRect)
-        pygame.display.update()
-        clock.tick(15)
-
-theme = input("Choose your theme: (space, midevill, star wars, original)")
-#class of bird
+#under imput allowing the user to choose the theme of the game 
+theme = input("Choose your theme: (space, beach, original, mario, pro) ")
+#class of FlappyBird
 class FlappyBird:
     def __init__(self):
         #size of playing screen 
         self.screen = pygame.display.set_mode((400, 708))
         #size of images
         self.bird = pygame.Rect(65, 50, 50, 50) 
+        #if statement depending on the user imput for the theme 
         if theme == "space": 
             #the background in the game from the folder 'assets'
-            self.background = pygame.image.load("assets/background.png").convert()
-            #the bird images
-            self.birdSprites = [pygame.image.load("assets/1.png").convert_alpha(),
+            self.background = pygame.image.load("assets/background2.png").convert()
+            #the images of the bird from assets 
+            # 1 is normal 
+            # 2 is falling 
+        # dead is the death image when the bird hit an obstacle 
+            self.birdSprites = [pygame.image.load("assets/mar1.png").convert_alpha(),
                                 pygame.image.load("assets/2.png").convert_alpha(),
                                 pygame.image.load("assets/dead.png")]
             #the obstacles in the game from the folder
             self.wallUp = pygame.image.load("assets/bottom.png").convert_alpha()
+    ##attempt at rolling background 
+    ##did not work, covered whole screen blocking out bird 
+            # def events():
+	        #     for event in pygame.event.get():
+		    #         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+			#             pygame.quit()
+			#             sys.exit()
+            # # define display surface			
+            # W, H = 576, 1024
+            # HW, HH = W / 2, H / 2
+            # AREA = W * H
+            # os.environ['SDL_VIDEO_WINDOW_POS'] = "50,50"
+            # # setup pygame
+            # pygame.init()
+            # CLOCK = pygame.time.Clock()
+            # DS = pygame.display.set_mode((W, H))
+            # pygame.display.set_caption("code.Pylet - Scrolling Background Image")
+            # FPS = 120
+            # bkgd = pygame.image.load("assets/background1.png").convert()
+            # x = 0
+            # # main loop
+            # while True:
+	        #     events()
+	        #     rel_x = x % bkgd.get_rect().width
+	        #     DS.blit(bkgd, (rel_x - bkgd.get_rect().width, 0))
+	        #     if rel_x < W:
+	        #     	DS.blit(bkgd, (rel_x, 0))
+	        #     x -= 1
+	        #     pygame.draw.line(DS, (255, 0, 0), (rel_x, 0), (rel_x, H), 3)
+	        #     pygame.display.update()
+	        #     CLOCK.tick(FPS)
             self.wallDown = pygame.image.load("assets/top.png").convert_alpha()
-        if theme == "original": 
-            #the background in the game from the folder 'assets'
+        if theme == "beach": 
             self.background = pygame.image.load("assets/background1.png").convert()
-            #the bird images
             self.birdSprites = [pygame.image.load("assets/1.png").convert_alpha(),
                                 pygame.image.load("assets/2.png").convert_alpha(),
                                 pygame.image.load("assets/dead.png")]
-            #the obstacles in the game from the folder
+            self.wallUp = pygame.image.load("assets/NObottom.png").convert_alpha()
+            self.wallDown = pygame.image.load("assets/NOtop.png").convert_alpha()
+        if theme == "original":
+            self.background = pygame.image.load("assets/background1.png").convert()
+            self.birdSprites = [pygame.image.load("assets/1.png").convert_alpha(),
+                                pygame.image.load("assets/2.png").convert_alpha(),
+                                pygame.image.load("assets/dead.png")]
+            self.wallUp = pygame.image.load("assets/NObottom.png").convert_alpha()
+            self.wallDown = pygame.image.load("assets/NOtop.png").convert_alpha()
+        if theme == "mario": 
+            self.background = pygame.image.load("assets/background1.png").convert()
+            self.birdSprites = [pygame.image.load("assets/1.png").convert_alpha(),
+                                pygame.image.load("assets/2.png").convert_alpha(),
+                                pygame.image.load("assets/dead.png")]
+            self.wallUp = pygame.image.load("assets/NObottom.png").convert_alpha()
+            self.wallDown = pygame.image.load("assets/NOtop.png").convert_alpha()
+        if theme == "pro": 
+            self.background = pygame.image.load("assets/background1.png").convert()
+            self.birdSprites = [pygame.image.load("assets/1.png").convert_alpha(),
+                                pygame.image.load("assets/2.png").convert_alpha(),
+                                pygame.image.load("assets/dead.png")]
             self.wallUp = pygame.image.load("assets/NObottom.png").convert_alpha()
             self.wallDown = pygame.image.load("assets/NOtop.png").convert_alpha()
         #changes the distance between obstacles  
@@ -68,7 +102,8 @@ class FlappyBird:
         self.sprite = 0 
         self.counter = 0
         self.offset = random.randint(-110, 110)
-
+        ##dead = pygame.mixer.Sound('deathsound')
+        ##dead.play()
     def updateWalls(self):
         self.wallx -= 5
         if self.wallx < -80:
@@ -111,7 +146,7 @@ class FlappyBird:
     def run(self):
         clock = pygame.time.Clock()
         pygame.font.init()
-        font = pygame.font.SysFont("Arial", 50)
+        font = pygame.font.SysFont("Arial", 60)
         while True:
             clock.tick(60)
             for event in pygame.event.get():
